5eac928e51595c02c784b461efb02d23e5515cac
David Borrego <dborrego@atsistemas.com>
2021-01-21 22:57:36 +0100

    test msg to mqtt server

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
 #include "ESP8266WiFi.h"
+#include <AsyncMqttClient.h>
 
 #define FC28_PIN A0
 #define DELAY 5000
 #define WIFI_SSID ""
 #define WIFI_PASS ""
+#define MQTT_HOST IPAddress(192, 168, 1, 10)
+#define MQTT_PORT 1883
 
 static const int MIN_RANGE = 0;
 static const int MAX_RANGE = 1023;
 static const int CEN = 100;
+
+AsyncMqttClient mqttClient;
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -21,6 +39,15 @@ void setup() {
 
   Serial.print("Conectado con IP: ");
   Serial.println(WiFi.localIP());
+
+  Serial.println("0");
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  Serial.println("1");
+  mqttClient.connect();
+  mqttClient.publish("test/one", 0, false, "a");
+  Serial.println("2");
 }
 
 void loop() {
@@ -31,5 +58,9 @@ void loop() {
   Serial.print(" - ");
   Serial.print(humidityPercent);
   Serial.println("%");
+  if(mqttClient.connected()){
+    Serial.println("conectado");
+    mqttClient.publish("/test/one", 0, true, String(humidity).c_str());
+  }
   delay(DELAY);
 }
